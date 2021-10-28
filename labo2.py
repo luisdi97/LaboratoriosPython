5388ed9d8e5249853f5f15205eb0d1ac7f65ec99
Luis Diego Araya Campos <luisdiegoarayacampos@hotmail.com>
2021-10-28 13:32:39 -0600

    Se hacen arreglos del estilo

--- labo2.py
+++ labo2.py
@@ -5,34 +5,40 @@ Autor: Luis Diego Araya Campos.
 Programa para darle formato correcto a los nombres.
 
 Este programa toma nombres de 3 o 4 componentes cuyos caracteres pertenezcan al
-alfabeto, es decir, no sean numeros ni caracteres especiales, y vuelve la
-primer letra de cada componente en mayuscula y el resto en minuscula.
+alfabeto, es decir, no sean números ni caracteres especiales, y vuelve la
+primer letra de cada componente en mayúscula y el resto en minúscula.
 '''
 
 nombres = []
 
 while(True):
-    BOOL_tam = True  # Defino dos booleanos como verdaderos
+    # Defino dos booleanos como verdaderos
+    BOOL_tam = True
     BOOL_esp = True
+
     nombre = input('Escriba un nombre, o SALIR para salir: ')
     if(nombre == 'SALIR'):
         break
     lista = nombre.split(' ')
     length = len(lista)
     if(not(length == 3 or length == 4)):
-        BOOL_tam = False  # Es falso si la cantidad de compontes esta mal
+        BOOL_tam = False  # Es falso si la cantidad de compontes está mal
     for n in range(length):
         if(not lista[n].isalpha()):
-            BOOL_esp = False  # Es falso si hay caracteres no alfabeticos
+            BOOL_esp = False  # Es falso si hay caracteres no alfabéticos
     if(not BOOL_tam and not BOOL_esp):  # Se presentan ambos errores
         print('ERROR 1: Debe ingresar nombres de 3 o 4 componentes')
-        print('ERROR 2: Debe ingresar nombres sin numeros o caracteres '
-              'especiales')
+        print(
+            'ERROR 2: Debe ingresar nombres sin números o caracteres '
+            'especiales'
+            )
     elif(not BOOL_tam):  # Se presenta el error de cantidad de componentes
         print('ERROR: Debe ingresar nombres de 3 o 4 componentes')
-    elif(not BOOL_esp):  # Se presenta el error de caracteres no alfabeticos
-        print('ERROR: Debe ingresar nombres sin numeros o caracteres '
-              'especiales')
+    elif(not BOOL_esp):  # Se presenta el error de caracteres no alfabéticos
+        print(
+            'ERROR: Debe ingresar nombres sin números o caracteres '
+            'especiales'
+            )
     else:  # Si no hay errores le da el formato correcto al nombre
         for n in range(length):
             lista[n] = lista[n].capitalize()
